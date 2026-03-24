83234d4e35c0962b41225101f0b5f9e998237918
E-denu <97678886+E-denu@users.noreply.github.com>
2026-03-24 08:42:42 +0000

    Change partition from teaching-gpu to econ-gpu

--- Cell_Count/slurm.sh
+++ Cell_Count/slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=logs/job_%A_%a.out
 #SBATCH --error=logs/job_%A_%a.err
 #SBATCH --array=1
-#SBATCH --partition=teaching-gpu           # Directs the job to the GPU queue             
+#SBATCH --partition=econ-gpu               
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32               # CPUs are still needed to move data to the GPU
+#SBATCH --cpus-per-task=32   
 #SBATCH --mem-per-cpu=2000
 
 
@@ -16,7 +16,6 @@ conda activate cellpose-env
 export LD_LIBRARY_PATH=/home/ed488/anaconda3/envs/cellpose-env/lib/python3.9/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-echo 'all exported'
 
 #module load cuda/12.1
 
